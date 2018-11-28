6ee124e615bc9526c35f360a1a2d3db118c6a20a
User <User@DESKTOP-3U5N3GB>
2018-11-28 10:54:05 +0800

    CUSTOMER MAINTENANCE AND INVOICE PAYMENT

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -9,7 +9,9 @@ package menu;
 import java.util.Scanner;
 import java.io.*;
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 /**
  *
  * @author User
@@ -21,19 +23,23 @@ public class Menu {
      */
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
+        ArrayList<String> ass = new ArrayList<String>();
         int loginSelect;
         do {
             loginSelection();
             loginSelect = input.nextInt();
 
             if (loginSelect == 1 || loginSelect == 2) {
-                if (loginSelect == 1) {
-                    LoginPassword();
-                } else if (loginSelect == 2) {
+                if (loginSelect == 1) 
+                {
+                    CustomerMaintain();
+                }
+                else if (loginSelect == 2) 
+                {
                     Register();
                 }
             } else {
-                System.out.println("\n !!!!!!Please Enter 1 OR 2 !!!!!!!!! \n");
+                System.err.println("\n !!!!!!Please Enter 1 OR 2 !!!!!!!!! \n");
             }
 
         } while (loginSelect != 1 && loginSelect != 2);
@@ -41,31 +47,23 @@ public class Menu {
 
     public static void loginSelection() {
 
-        System.out.println("==================\n Flower Shop \n================");
-        System.out.println("1. LOGIN");
+        System.out.println("==================\n Flower Shop \n==================");
+        System.out.println("1. CUSTOMER MAINTIAN");
         System.out.println("2. REGISTER");
 
         System.out.print("Please Enter Your Selection : ");
 
     }
 
-    public static void LoginPassword() {
-        String UserName;
-        String Password;
-        Scanner Verifylogin = new Scanner(System.in);
-        System.out.println("============\n LOGIN \n ============");
-        System.out.print("USERNAME  : ");
-        UserName = Verifylogin.nextLine();
-        System.out.print("PASSWORD  : ");
-        Password = Verifylogin.nextLine();
-    }
 
     public static void Register() throws Exception {
         String RUserName;
         String RIC;
         String PhoneNumber;
         int CustomerType;
-        int CreditLimit;
+        String CreditLimit;
+        String Address;
+        String CompanyName;
         Scanner Register = new Scanner(System.in);
         Scanner Ctype = new Scanner(System.in);
         System.out.println("==============\n Register \n===============");
@@ -75,6 +73,8 @@ public class Menu {
         RIC = Register.nextLine();
         System.out.print("Phone Number : ");
         PhoneNumber = Register.nextLine();
+        System.out.print("Please Enter Address : ");
+        Address = Register.nextLine();
         System.out.println("CUSTOMER TYPE : ");
         System.out.println("1. Consumer  ");
         System.out.println("2. Corporate Customer ");
@@ -86,19 +86,22 @@ public class Menu {
                     File consumer = new File("Consumer.txt");
                     FileWriter fileWritterConsumer = new FileWriter(consumer.getName(), true);
                     PrintWriter printWriterConsumer = new PrintWriter(fileWritterConsumer);
-                    printWriterConsumer.println(RUserName + "," + RIC + "," + PhoneNumber + "," + "Consumer");
+                    printWriterConsumer.println(RUserName + "," + RIC + "," + PhoneNumber + "," + Address + ",");
                     printWriterConsumer.flush();
                     printWriterConsumer.close();
                 } 
                 
                 if (CustomerType == 2) {
                 
-                System.out.print("Enter Monthly Credit Limit : ");
-                CreditLimit = Ctype.nextInt();
+                System.out.print("Enter Monthly Credit Limit  : ");
+                CreditLimit = Register.nextLine();
+                System.out.print("Enter Company Name          : ");
+                CompanyName = Register.nextLine();
+                
                 File corporate = new File("Corporate.txt");
                 FileWriter fileWritterCorporate = new FileWriter(corporate.getName(), true);
                 PrintWriter printWriterCorporate = new PrintWriter(fileWritterCorporate);
-                printWriterCorporate.println(RUserName + "," + RIC + "," + PhoneNumber + "," + "Corporate" + "," + CreditLimit);
+                printWriterCorporate.println(RUserName + "," + RIC + "," + PhoneNumber + "," + Address + "," + CreditLimit + "," + CompanyName + ",");
                 printWriterCorporate.flush();
                 printWriterCorporate.close();    
                 }
@@ -111,41 +114,198 @@ public class Menu {
         } while (CustomerType != 1 && CustomerType != 2);
 
     }
+    
+    public static void CustomerMaintain() throws Exception 
+    {
+        int CusMaintainSelect;
+        
+        do {
+        
+        Scanner Custmaintain = new Scanner(System.in);
+        System.out.println("1. Corporate Customer List ");
+        System.out.println("2. Consumer  Customer List ");
+        System.out.println("3. Search by IC            ");
+        System.out.println("4. EXIT                    ");
+        System.out.print("\n Please Enter Your Selection : ");
+        CusMaintainSelect = Custmaintain.nextInt();
+            if (CusMaintainSelect == 1 || CusMaintainSelect == 2 || CusMaintainSelect == 3 || CusMaintainSelect == 4) 
+                
+            {
+                if (CusMaintainSelect == 1) 
+                {
+                    File CorporateFile = new File("Corporate.txt");
+                    Scanner inputCusCorporate = new Scanner(CorporateFile);
 
-//    public static void CustomerType() throws Exception {
-//        Scanner Ctype = new Scanner(System.in);
-//        int CustomerType;
-//        int CreditLimit;
-//        System.out.println("CUSTOMER TYPE : ");
-//        System.out.println("1. Consumer  ");
-//        System.out.println("2. Corporate Customer ");
-//        System.out.print("Please Enter Your Selection : ");
-//        CustomerType = Ctype.nextInt();
-//        do {
-//            if (CustomerType == 1 || CustomerType == 2) {
-//                if (CustomerType == 1) {
-//                    File consumer = new File("Consumer.txt");
-//                    FileWriter fileWritterConsumer = new FileWriter(consumer.getName(), true);
-//                    PrintWriter printWriterConsumer = new PrintWriter(fileWritterConsumer);
-////                    printWriterConsumer.print(RUserName);
-//                    printWriterConsumer.flush();
-//                    printWriterConsumer.close();
-//                } 
-//                
-//                if (CustomerType == 2) {
-//                
-//                System.out.print("Enter Monthly Credit Limit : ");
-//                CreditLimit = Ctype.nextInt();
-////            File CorporateCustomer = new File("CorporateCustomer.txt");
-//                }
-//
-//            } 
-//            
-//            else {
-//                System.out.println("\n !!!!!!Please Enter 1 OR 2 !!!!!!!!! \n");
-//            }
-//        } while (CustomerType != 1 && CustomerType != 2);
-//    }
+                    inputCusCorporate.useDelimiter(",");
+                    
+                    System.out.println("*************************************************************************************************************************************************");
+                    System.out.printf("** %-20s ** %-20s ** %-20s ** %-20s ** %-20s ** %-20s ** \n"," NAME","IC","PHONE NUMBER","ADDRESS","CREDIT LIMIT","COMPANY ");
+                    System.out.println("=================================================================================================================================================");
+
+                    while (inputCusCorporate.hasNext()) 
+                    {
+                        String cORPORATE_NAME = inputCusCorporate.next();
+                        String cORPORATE_IC = inputCusCorporate.next();
+                        String cORPORATE_PN = inputCusCorporate.next();
+                        String cORPORATE_Address = inputCusCorporate.next();
+                        String cORPORATE_Credit = inputCusCorporate.next();
+                        String cORPORATE_Company = inputCusCorporate.next();
+                        
+                        String next = inputCusCorporate.nextLine();
+                        System.out.printf("** %-20s ** %-20s ** %-20s ** %-20s ** %-20s ** %-20s ** \n", cORPORATE_NAME,cORPORATE_IC,cORPORATE_PN,cORPORATE_Address,cORPORATE_Credit,cORPORATE_Company);
+
+                    }
+                }
+                if (CusMaintainSelect == 2) 
+                {
+                    File ConsumerFile = new File("Consumer.txt");
+                    Scanner inputCusConsumer = new Scanner(ConsumerFile);
+
+                    inputCusConsumer.useDelimiter(",");
+                    
+                    System.out.println("*********************************************************************************************************************");
+                    System.out.printf("** %-20s ** %-20s ** %-20s ** %-20s **\n"," NAME","IC","PHONE NUMBER","ADDRESS");
+                    System.out.println("=====================================================================================================================");
+                    
+                    while (inputCusConsumer.hasNext()) 
+                    {
+                        String cONSUMER_NAME = inputCusConsumer.next();
+                        String cONSUMER_IC = inputCusConsumer.next();
+                        String cONSUMER_PN = inputCusConsumer.next();
+                        String cONSUMER_Address = inputCusConsumer.next();
+                        
+                         String next = inputCusConsumer.nextLine();
+                        
+                        System.out.printf("** %-20s ** %-20s ** %-20s ** %-20s **\n", cONSUMER_NAME,cONSUMER_IC,cONSUMER_PN,cONSUMER_Address);
+                    }
+
+                }
+                if (CusMaintainSelect == 3) 
+                {
+                    Search();
+                } 
+            }
+            else 
+                {
+                    System.err.print("\nPlease Enter A Valid Number (1-4) \n\n");
+                }
+        }while (CusMaintainSelect != 1 && CusMaintainSelect != 2 && CusMaintainSelect != 3 && CusMaintainSelect != 4);
+        
+    }
+    ;
+
+    public static void Search() throws Exception
+    {
+        int SearchSelection;
+        String Name = null;
+        String ShowIC = null;
+        String PN = null;
+        String Address = null;
+        String CreditLimit = null;
+        String Company = null;
+        boolean flag = false;
+        Scanner SSearch = new Scanner(System.in);
+        
+        do {
+            System.out.println("1. Corporate Customer   ");
+            System.out.println("2. Consumer  Customer   ");
+            System.out.println("3. EXIT                    ");
+            System.out.print("\n Please Enter Your Selection : ");
+            SearchSelection = SSearch.nextInt();
+            if (SearchSelection == 1 || SearchSelection == 2 || SearchSelection == 3) {
+                if(SearchSelection == 1)
+                {
+                    String IC;
+                    Scanner SearchIC = new Scanner(System.in);
+                    System.out.print("Please Enter IC   :    ");
+                    IC = SearchIC.next();
+                    File ConsumerFile = new File("Corporate.txt");
+                    Scanner inputCusCorporate = new Scanner(ConsumerFile);
+                    
+                    inputCusCorporate.useDelimiter(",");
+                    
+                    while ( inputCusCorporate.hasNext()) 
+                    {
+                        String cORPORATE_NAME = inputCusCorporate.next();
+                        String cORPORATE_IC = inputCusCorporate.next();
+                        String cORPORATE_PN = inputCusCorporate.next();
+                        String cORPORATE_Address = inputCusCorporate.next();
+                        String cORPORATE_Credit = inputCusCorporate.next();
+                        String cORPORATE_Company = inputCusCorporate.next();
+                       
+                        String next = inputCusCorporate.nextLine();
+                       
+                        if (IC.toString().compareTo(cORPORATE_IC.toString())==0)
+                        {
+                            
+                            System.out.println("NAME         : " + cORPORATE_NAME);
+                            System.out.println("IC           : " + cORPORATE_IC);
+                            System.out.println("Phone Number : " + cORPORATE_PN);
+                            System.out.println("Address      : " + cORPORATE_Address);
+                            System.out.println("Credit Limit : " + cORPORATE_Credit);
+                            System.out.println("Company Name : " + cORPORATE_Company);
+                            flag = true;
+                            break;
+                        }
+                        
+                    }
+                   if(flag==false)
+                        {
+                            System.err.println("Sorry , This IC is NOT Exist ");
+                        }
+                        
+                    
+
+                }
+                if(SearchSelection == 2)
+                {
+                    String IC;
+                    Scanner SearchIC = new Scanner(System.in);
+                    System.out.print("Please Enter IC   :    ");
+                    IC = SearchIC.next();
+                    File ConsumerFile = new File("Consumer.txt");
+                    Scanner inputCusCorporate = new Scanner(ConsumerFile);
+                    
+                    inputCusCorporate.useDelimiter(",");
+                    
+                    while ( inputCusCorporate.hasNext()) 
+                    {
+                        String cORPORATE_NAME = inputCusCorporate.next();
+                        String cORPORATE_IC = inputCusCorporate.next();
+                        String cORPORATE_PN = inputCusCorporate.next();
+                        String cORPORATE_Address = inputCusCorporate.next();
+                       
+                        String next = inputCusCorporate.nextLine();
+                       
+                        if (IC.toString().compareTo(cORPORATE_IC.toString())==0)
+                        {
+                            
+                            System.out.println("NAME         : " + cORPORATE_NAME);
+                            System.out.println("IC           : " + cORPORATE_IC);
+                            System.out.println("Phone Number : " + cORPORATE_PN);
+                            System.out.println("Address      : " + cORPORATE_Address);
+                            flag = true;
+                            break;
+                        }
+                        
+                    }
+                   if(flag==false)
+                        {
+                            System.err.println("Sorry , This IC is NOT Exist ");
+                        }
+                }
+                if(SearchSelection == 3)
+                {
+                    
+                }
+
+            } 
+            else 
+            {
+                System.out.println("\nPlease Enter A Valid Number (1-4) \n\n");
+            }
+        }while (SearchSelection != 1 && SearchSelection != 2 && SearchSelection != 3);
+    }
 
     public static void ConfirmStatement() {
         int confirm;
